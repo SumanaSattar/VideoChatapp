009c0c3efc6c7142f5e32053abe290434c99fff0
Sumana Sattar <50830293+SumanaSattar@users.noreply.github.com>
2020-11-29 13:01:37 +0100

    peer is fixed

--- public/script.js
+++ public/script.js
@@ -1,7 +1,26 @@
 
 const socket = io('/')
+const videoGrid = document.getElementById('video-grid')
+const myPeer = new Peer(undefined, {
+    host:'/',
+    port:'3001'
+})
+const myVideo = document.createElement('video')
+myVideo.muted = true
 
-socket.emit('join-room', ROOM_ID ,10)
+navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: true
+  }).then(stream => {
+
+  })
+
+myPeer.on('open', id => {
+    socket.emit('join-room', ROOM_ID ,id)
+})
+
+
+
 
 socket.on('user-connected',userId => {
     console.log('User Connected:'+ userId)
